f20513e98bf266c88a1c467227cd2312475300ad
Linus David Martinez Garcia <linusmartinez00@gmail.com>
2025-11-22 23:15:02 -0600

    Hotfix

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -2,8 +2,8 @@ import { spawn } from "child_process";
 
 export function runAI(inputData) {
   return new Promise((resolve, reject) => {
-    const py = spawn("python3", ["src/ai_predict.py"], {
-      cwd: process.cwd() // asegura ruta correcta
+    const py = spawn("python3", ["ai_predict.py"], {
+      cwd: path.join(process.cwd(), "src") // asegura ruta correcta
     });
 
     let output = "";
